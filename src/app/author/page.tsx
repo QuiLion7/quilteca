1a75619e3b1f94a02ed2205dd0448170c6767042
QuiLion7 <quilbrub@gmail.com>
2024-04-10 14:23:34 -0300

    feat: Create Author page

---
+++ src/app/author/page.tsx
@@ -0,0 +1,7 @@
+export default function Author() {
+  return (
+    <main className="flex w-full items-center justify-center bg-pink-600 px-2 2xl:container">
+      Tela Registrar Autor/Banca em Construção
+    </main>
+  );
+}
