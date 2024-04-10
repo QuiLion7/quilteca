0c8ba6f76573e5161dfa26197699265f6d272af4
QuiLion7 <quilbrub@gmail.com>
2024-04-10 14:26:30 -0300

    feat: Create Search page

---
+++ src/app/search/page.tsx
@@ -0,0 +1,7 @@
+export default function Search() {
+  return (
+    <main className="flex w-full items-center justify-center bg-blue-600 px-2 2xl:container">
+      Tela Buscar Item em Construção
+    </main>
+  );
+}
