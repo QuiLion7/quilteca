6ef4909d77a2adc8ebb443c67c8f918dde7f82b0
QuiLion7 <quilbrub@gmail.com>
2024-04-10 14:23:16 -0300

    feat: Change display text on construction notice of home page

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex w-full items-center justify-center bg-secondary px-2 2xl:container">
+      Tela Inicial em Construção
+    </main>
+  );
+}
