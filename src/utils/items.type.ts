6ca7ddfb76b4fdf24e549e578c0097f4139652dd
QuiLion7 <quilbrub@gmail.com>
2024-04-10 14:30:17 -0300

    feat: Create TypeScript typings file for items named items.type.ts

---
+++ src/utils/items.type.ts
@@ -0,0 +1,23 @@
+export interface Alternative {
+  alternative: string;
+  correct: boolean;
+}
+
+export interface ItemProps {
+  id: string;
+  knowledge: string;
+  componentCurricular: string;
+  subject: string;
+  author: string;
+  year: number;
+  bNCCCompetence: string;
+  bNCCHability: string;
+  difficultyLevel: string;
+  baseText?: string | null;
+  resource?: string | null;
+  itemCommand: string;
+  alternatives: Alternative[] | null;
+  createdAt: Date | null;
+  updateAt: Date | null;
+  userId: string | null;
+}
