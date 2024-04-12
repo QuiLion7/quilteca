d1043019e7041b1e33865d4b86a96087db27a392
QuiLion7 <quilbrub@gmail.com>
2024-04-12 06:45:24 -0300

    feat(page): Add router refresh functionality and showItem state management; Implement handleDeleteItem function for item deletion

--- src/app/myitems/components/card/index.tsx
+++ src/app/myitems/components/card/index.tsx
@@ -1,10 +1,39 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { api } from "@/lib/api";
 import { ItemProps } from "@/utils/items.type";
-import { Pencil, Trash, ZoomIn } from "lucide-react";
+import { Pencil, Trash, ZoomIn, ZoomOut } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export function CardItem({ item }: { item: ItemProps }) {
+  const router = useRouter();
+
+  const [showItem, setShowItem] = useState(false);
+
+  useEffect(() => {
+    router.refresh();
+  }, [router]);
+
+  async function handleDeleteItem() {
+    try {
+      const response = await api.delete("/api/items", {
+        params: {
+          id: item.id,
+        },
+      });
+      console.log(response.data);
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <article className="flex h-auto w-screen flex-col justify-between gap-2 rounded-lg border-2 bg-secondary p-2 duration-300">
+    <article className="flex h-auto w-full flex-col justify-between gap-2 rounded-lg border-2 bg-secondary/30 p-2 duration-300 hover:bg-secondary">
       <article className="flex w-full flex-wrap items-center justify-center gap-2 text-justify">
         <h2>
           <strong>Assunto:</strong> {item.subject} / {item.specificSubject}
@@ -26,33 +55,44 @@ export function CardItem({ item }: { item: ItemProps }) {
         </p>
       </article>
       <section className="flex flex-col items-start justify-center gap-2 text-justify">
-        <p>
-          <strong>Texto Base:</strong> {item.baseText}
-        </p>
-        <p>
-          <strong>Recurso:</strong> {item.resource}
-        </p>
+        {showItem && (
+          <div>
+            <p>
+              <strong>Texto Base:</strong> {item.baseText}
+            </p>
+            <p>
+              <strong>Recurso:</strong> {item.resource}
+            </p>
+          </div>
+        )}
         <p>
           <strong>Comando:</strong> {item.itemCommand}
         </p>
       </section>
-      <ul>
-        {item.alternatives?.map((alternative, index) => (
-          <li
-            key={index}
-            className={alternative.correct ? "font-bold text-green-500" : ""}
-          >
-            {`${String.fromCharCode(65 + index)}) ${alternative.alternative}`}
-          </li>
-        ))}
-      </ul>
+      {showItem && (
+        <ul className="flex w-full flex-col items-start justify-center gap-2 border-y-2 border-secondary py-2 text-justify">
+          {item.alternatives?.map((alternative, index) => (
+            <li
+              key={index}
+              className={alternative.correct ? "font-bold text-green-500" : ""}
+            >
+              {`${String.fromCharCode(65 + index)}) ${alternative.alternative}`}
+            </li>
+          ))}
+        </ul>
+      )}
 
       <div className="flex items-center justify-end gap-2">
         <Button
           variant="info"
+          onClick={() => setShowItem(!showItem)}
           className="h-auto w-auto p-1 transition-all duration-300 hover:scale-110"
         >
-          <ZoomIn className="h-auto w-6" />
+          {showItem === false ? (
+            <ZoomIn className="h-auto w-6" />
+          ) : (
+            <ZoomOut className="h-auto w-6" />
+          )}
         </Button>
         <Button
           variant="editable"
@@ -62,6 +102,7 @@ export function CardItem({ item }: { item: ItemProps }) {
         </Button>
         <Button
           variant="destructive"
+          onClick={() => handleDeleteItem()}
           className="h-auto w-auto p-1 transition-all duration-300 hover:scale-110"
         >
           <Trash className="h-auto w-6" />
